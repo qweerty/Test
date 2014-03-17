bdc84210caee9b8d9cea46eadcb8185881afccc9
Евгений Привалов <saqel@yandex.ru>
2014-03-17 11:01:09 -0700

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,12 @@
+#lmlamdd,a;d,;asd
+#kllkdamdlamdasda
+#KLsmlkdmaslkdmaksdmasda asdaskdmasda
+#kmdlkamdamdaml
+#2014
+
+
+from Tkinter import *
+
+#Creating a new window
+root = Tk()
+root.mainloop()
